410ec1ff4d8f1b004a3124873548f75b71f253cb
wangy@workai.com.cn <wangy@workai.com.cn>
2017-07-12 20:31:05 +0800

    添自定义view之圆环（不同颜色填充）

--- app/src/main/java/com/mardawang/android/mypracticedraw/widget/MyBreadView.java
+++ app/src/main/java/com/mardawang/android/mypracticedraw/widget/MyBreadView.java
@@ -10,6 +10,8 @@ import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.mardawang.android.mypracticedraw.R;
+
 /**
  * Created by mardawang on 2017/7/11.
  */
@@ -73,5 +75,27 @@ public class MyBreadView extends View {
         paint1.setColor(Color.BLUE);
         canvas.drawArc(300,300,800,800,170,10,true,paint1);
 
+        //圆环
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.GREEN);
+        canvas.drawArc(300,1000,700,1400,-180,110,true,paint);
+        paint.setColor(Color.RED);
+        canvas.drawArc(300,1000,700,1400,-70,20,true,paint);
+        paint.setColor(Color.BLACK);
+        canvas.drawArc(300,1000,700,1400,-50,70,true,paint);
+        paint.setColor(Color.YELLOW);
+        canvas.drawArc(300,1000,700,1400,20,80,true,paint);
+        paint.setColor(Color.BLUE);
+        canvas.drawArc(300,1000,700,1400,100,30,true,paint);
+        paint.setColor(Color.GRAY);
+        canvas.drawArc(300,1000,700,1400,130,50,true,paint);
+
+        paint.setColor(getResources().getColor(R.color.color_gray6));
+        canvas.drawCircle(500,1200,170,paint);
+
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(44);
+//        canvas.drawText("1453 calories\n burned",380,1200,paint);
+        canvas.drawText("android is a nice\n language",380,1200,paint);
     }
 }
